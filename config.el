49dc60d89289e722d0d3aa17815b4163ead359d2
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2023-07-09 10:31:12 +0200

    Add initial doom config

---
+++ config.el
@@ -0,0 +1,125 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+(setq doom-leader-key "\\"
+      doom-localleader-key "\\ m")
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "Elia Camposilvan"
+      user-mail-address "elia.camposilvan@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'leuven)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Library/CloudStorage/Dropbox/org")
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+(setq confirm-kill-emacs nil)
+
+(setq org-startup-indented t
+      org-pretty-entities t
+      org-hide-emphasis-markers t
+      org-startup-with-inline-images t
+      org-image-actual-width '(300))
+
+(use-package org-superstar
+  :config
+  (setq org-superstar-special-todo-items t)
+  (add-hook 'org-mode-hook (lambda ()
+                             (org-superstar-mode 1))))
+
+(setq org-todo-keywords '
+      ((sequence "TODO" "IN PROGRESS" "|" "DONE")))
+
+(defun my-go-to-org-root ()
+  "Go to the root folder of Org files."
+  (interactive)
+  (find-file org-directory))
+
+(map!
+ :desc "Find file in project" "C-p" #'projectile-find-file
+ :desc "Format buffer/region" "C-f" #'+format/region-or-buffer
+ :leader
+ :desc "Search project" "ff" #'+default/search-project
+ :desc "Find file" "fb" #'find-file
+ :desc "Recent files" "fo" #'recentf-open-files
+ :desc "Show buffers" "o" #'ibuffer
+ :desc "Find org file" "fw" #'my-go-to-org-root
+ )
+
+(use-package org-download
+  :after org
+  :demand t
+  :commands (org-download-enable
+             org-download-yank
+             org-download-screenshot)
+  :config
+  (setq org-download-method 'directory)
+  (setq org-download-image-dir "./img")
+  (setq org-download-screenshot-method "screencapture -i %s")
+  :hook ((org-mode dired-mode) . org-download-enable))
